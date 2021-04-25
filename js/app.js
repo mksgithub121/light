9f907b286d2443f9e5f46e4bc62ba1ec936614f4
Mahadi <mksbd.net@gmail.com>
2021-04-25 22:56:03 +0600

    header responsive

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var fixHeight = function () {
+        $('.navbar-nav').css(
+            'max-height',
+            document.documentElement.clientHeight - 150
+        );
+    };
+    fixHeight();
+    $(window).resize(function () {
+        fixHeight();
+    });
+    $('.navbar .navbar-toggler').on('click', function () {
+        fixHeight();
+    });
+    $('.navbar-toggler').on('click', function () {
+        $('.mobileMenu').toggleClass('open');
+    });
+    $(" .src_btn").on("click", function () {
+        $(".search_wrapper").toggleClass("search_active");
+    });
+    $(" .cart_btn").on("click", function () {
+        $(".cart_full").toggleClass("cart_active");
+    });
+});
