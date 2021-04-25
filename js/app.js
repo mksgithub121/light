8943f6b21ada5d13e748cc293e2db98d0756898b
Mahadi <mksbd.net@gmail.com>
2021-04-26 00:34:30 +0600

    add

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     });
     $('.navbar-toggler').on('click', function () {
         $('.mobileMenu').toggleClass('open');
+        $("body").css("overflow", "hidden");
     });
     $(" .src_btn").on("click", function () {
         $(".search_wrapper").toggleClass("search_active");
